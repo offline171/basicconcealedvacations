554efd3a24c8982d935e3fde00c1d1d23e065c9c
offline171 <offline.stein@gmail.com>
2025-04-19 23:54:03 -0400

    Revert "only adding astro node this time"
    This reverts commit 68ed0f1bb913903cc74eaadca97e93599dcc2f83.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,9 @@ import sitemap from '@astrojs/sitemap';
 
 import netlify from '@astrojs/netlify';
 
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: netlify(),
 });
