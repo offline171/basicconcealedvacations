486c235ad7c6d97e92fe53c4b0901b83eadf2466
offline171 <offline.stein@gmail.com>
2025-04-19 23:34:26 -0400

    Adding more dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,13 @@ import sitemap from '@astrojs/sitemap';
 
 import netlify from '@astrojs/netlify';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
-  adapter: netlify(),
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
